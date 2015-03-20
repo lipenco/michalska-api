73381e79fdd6ffe3243a79ceb7b380aa26c9367f
Magdalena Lipińska <mag.lipinska@gmail.com>
2015-03-20 15:01:50 +0800

    add user test

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,10 @@
 FactoryGirl.define do
   factory :user do
-    
+    email "lala@lala.com"
+    password "12345678"
+    password_confirmation "12345678"
+
+
   end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before { @user = FactoryGirl.build(:user) }
+
+    describe "user" do
+      subject { @user }
+
+      it { should respond_to(:email) }
+      it { should respond_to(:password) }
+      it { should respond_to(:password_confirmation) }
+
+      it { should be_valid }
+    end
+
+
+    describe "when email is not present" do
+      it { should validate_presence_of(:email) }
+      it { should validate_uniqueness_of(:email) }
+      it { should validate_confirmation_of(:password) }
+      it { should allow_value('example@domain.com').for(:email) }
+    end
+
+
 end
