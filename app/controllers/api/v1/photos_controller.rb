b2819e89a665f078da45d70bf3c094d50b54ed71
Magdalena Lipińska <mag.lipinska@gmail.com>
2015-03-22 13:25:55 +0800

    refactors photos controller

--- app/controllers/api/v1/photos_controller.rb
+++ app/controllers/api/v1/photos_controller.rb
@@ -1,33 +1,32 @@
 class Api::V1::PhotosController < ApplicationController
   respond_to :json
+  before_action :setUpProject, only: [:create, :update, :destroy]
+
 
    def index
      respond_with Project.find(params[:project_id]).photos
    end
 
    def create
-     project = current_user.projects.find(params[:project_id])
-     photo = project.photos.build(photo_params)
+     photo = @project.photos.build(photo_params)
      if photo.save
-      render json: photo , status: 201, location: [:api, project ]
+      render json: photo , status: 201, location: [:api, @project ]
      else
        render json: { errors: photo.errors }, status: 422
      end
    end
 
    def update
-     project = current_user.projects.find(params[:project_id])
-     photo = project.photos.find(params[:id])
+     photo = @project.photos.find(params[:id])
      if photo.update(photo_params)
-       render json: photo, status: 200, location: [:api, project]
+       render json: photo, status: 200, location: [:api, @project]
      else
        render json: { errors: photo.errors }, status: 422
      end
    end
 
    def destroy
-     project = current_user.projects.find(params[:project_id])
-     photo = project.photos.find(params[:id])
+     photo = @project.photos.find(params[:id])
      photo.destroy
      head 204
    end
@@ -37,6 +36,10 @@ class Api::V1::PhotosController < ApplicationController
 
    def photo_params
      params.require(:photo).permit(:url, :description, :horizontal)
+   end
+
+   def setUpProject
+     @project = current_user.projects.find(params[:project_id])
    end
 
 end
