504814e717c0cbad430c45853a26727868e50f21
Magdalena Lipińska <mag.lipinska@gmail.com>
2015-08-31 18:48:20 +0100

    token fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :auth_token
   before_create :generate_authentication_token!
 
   has_many :projects
