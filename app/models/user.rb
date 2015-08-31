c0e1816965d7672f8caf4095aadb33b3ac1f1f5d
Magdalena Lipińska <mag.lipinska@gmail.com>
2015-08-31 18:41:09 +0100

    fix token bug

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  validates :auth_token, uniqueness: true
+  validates :auth_token
   before_create :generate_authentication_token!
 
   has_many :projects
